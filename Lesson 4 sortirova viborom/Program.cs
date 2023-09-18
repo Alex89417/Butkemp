dfe0eb911e329ff5ebbfc6fe9b5f4efe05555c69
Alex89417 <zezylia25@gmail.com>
2023-09-18 09:12:57 +0300

    Добавлен lesson 4 и простой пример сортировки выбором

---
+++ Lesson 4 sortirova viborom/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+int[] array = {2, 7, -3, 8, 2, 1};
+
+void SortSelection(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < array.Length; j++)
+            if(array[j] < array[min]) min = j;
+        
+        int temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+    }
+}
+
+Console.WriteLine($"[{String.Join(',', array)}]");
+SortSelection(array);
+Console.WriteLine($"[{String.Join(',', array)}]");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
