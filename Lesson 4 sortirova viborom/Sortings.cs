e1f87499610213bf956301a37c380a28a96116bd
Alex89417 <zezylia25@gmail.com>
2023-09-23 12:41:45 +0300

    Доавил измеения в 4 урок и доавил папку урок 8

--- Lesson 4 sortirova viborom/Sortings.cs
+++ Lesson 4 sortirova viborom/Sortings.cs
@@ -7,7 +7,9 @@ public static class Sortings
             int min = i;
             for (int j = i + 1; j < array.Length; j++)
                 if(array[j] < array[min]) min = j;
-        
+            if (array[min] == array[j])
+                continue;
+                
             int temp = array[i];
             array[i] = array[min];
             array[min] = temp;
